dc095cbddd97aa42465c825145a83f32e85feabc
Kate-Hooker <katehooker88@gmail.com>
2024-08-08 20:26:17 +1200

    added css for main image

--- NASA-react-app/src/components/SideBar.jsx
+++ NASA-react-app/src/components/SideBar.jsx
@@ -1,12 +1,18 @@
 export default function SideBar() {
   return (
-    <div>
-      <h1>SideBar</h1>
-      <h2>Ground control to major tom</h2>
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h1>SideBar</h1>
+        <h2>Ground control to major tom</h2>
 
-      <div>
-        <p>Descriptoion:</p>
-        <p>Full length decsription</p>
+        <div>
+          <p>Descriptoion:</p>
+          <p>Full length decsription</p>
+        </div>
+        <button>
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
       </div>
     </div>
   )
