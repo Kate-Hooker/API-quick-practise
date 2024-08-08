ddbb155bab8e0f1a8f002b85896771ab7b95c68e
Kate-Hooker <katehooker88@gmail.com>
2024-08-08 15:30:25 +1200

    sidebar populated with placeholder data

--- NASA-react-app/src/components/SideBar.jsx
+++ NASA-react-app/src/components/SideBar.jsx
@@ -1,9 +1,13 @@
-export default function SideBar() {  
-
+export default function SideBar() {
   return (
-
     <div>
       <h1>SideBar</h1>
+      <h2>Ground control to major tom</h2>
+
+      <div>
+        <p>Descriptoion:</p>
+        <p>Full length decsription</p>
+      </div>
     </div>
   )
 }
