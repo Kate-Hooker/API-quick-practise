61c440a981d796f1e734f23945cb6c7e4f68c776
Kate-Hooker <katehooker88@gmail.com>
2024-08-09 19:39:47 +1200

    try catch used to get API data to display

--- NASA-react-app/src/App.jsx
+++ NASA-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import Main from './components/Main'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 function App() {
   /* const [variable with state, function to change state and set new value of variable] = useState(value variable starts at)*/
   const [showModal, setShowModal] = useState(false)
@@ -10,11 +10,28 @@ function App() {
     setShowModal(!showModal)
   }
 
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`
+
+      try {
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log('DATA\n', data)
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+
+    fetchAPIData()
+  }, [])
+
   return (
     <div>
       <Main />
 
-      {showModal && <SideBar handleToggleModal={handleToggleModal}/>}
+      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
       <Footer showModal={handleToggleModal} />
     </div>
     /* the showModal && booolean means if showModal is true show the SideBar
