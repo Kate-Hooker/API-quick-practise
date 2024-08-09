aa6475e3afd8993540de722eeb1c7b4551c1d3b7
Kate-Hooker <katehooker88@gmail.com>
2024-08-09 16:14:19 +1200

    useState added to open/close sidebar

--- NASA-react-app/src/App.jsx
+++ NASA-react-app/src/App.jsx
@@ -1,17 +1,19 @@
-import Main from "./components/Main"
-import Footer from "./components/Footer"
-import SideBar from "./components/SideBar"
+import Main from './components/Main'
+import Footer from './components/Footer'
+import SideBar from './components/SideBar'
+import { useState } from 'react'
 function App() {
-
+  /* const [variable with state, function to change state and set new value of variable] = useState(value variable starts at)*/
+  const [showModal, setShowModal] = useState(false)
 
   return (
-
     <div>
-      <Main/>
-      <SideBar>HI IM A SIDEBAR TITLE</SideBar>
-      <Footer/>
-      
+      <Main />
+
+      {showModal && <SideBar />}
+      <Footer />
     </div>
+    /* the showModal && booolean means if showModal is true show the SideBar */
   )
 }
 
